3faaac62573211bbceb5697fc65a30fa8d73be29
yoon <o_ink@naver.com>
2019-11-20 23:02:34 +0900

    Create account, Request Secret, Confirm Ssecret complete

--- src/server.js
+++ src/server.js
@@ -1,15 +1,20 @@
 require("dotenv").config();
+import dotenv from "dotenv";
+import path from "path";
+dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 import { GraphQLServer } from "graphql-yoga";
 import logger from "morgan";
+import passport from "passport";
 import schema from "./schema";
+import "./passport";
 
 const PORT = process.env.PORT || 4000;
 
-const server = new GraphQLServer({schema});
-
+const server = new GraphQLServer({ schema });
 server.express.use(logger("dev"));
+server.express.use(passport.authenticate("jwt"));
 
-server.start({port: PORT}, () => 
-console.log(`✅ Server running on http://localhost:${PORT}`)
+server.start({ port: PORT }, () =>
+  console.log(`✅ Server running on http://localhost:${PORT}`)
 );
